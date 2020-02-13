8cc1ae5ee504fc8f103267947767491ab999c49c
Flavio Ragni <47398415+flavioragni@users.noreply.github.com>
2020-02-13 15:09:59 +0100

    Add files via upload

---
+++ analyses/MVPA/cluster/rs_project_searchlight_nchoosek_CLUSTER_job_submit.m
@@ -0,0 +1,20 @@
+%% A Sort of preamble
+clear all;
+matlab_version = sscanf(version('-release'),'%d%s');
+addpath(genpath('\\CIMEC-STORAGE\anglin\LINANG001QX2\flavio\LnifClusterFunctions'));
+lnif_cluster = getLnifCluster('\\CIMEC-STORAGE\anglin\LINANG001QX2\flavio\matlab_pe','/home/flavio.ragni/matlab_pe');
+job = lnif_cluster.createJob('Name','cosmo_searchlight');
+job.AttachedFiles = {'rs_project_runsearchlight_nchoosek_CLUSTER.m'};
+%% Assign tasks using your function
+subList=[1,3,5,6,7,9,10,11,12,13,14,15,16,17,18,20,21,22,23,24,25,27,28];
+condition={'PLACE','FACE','FAMILIAR','FACE_PLACE','FACE_PLACE_FAMILIARITY'};
+for iSub=1:length(subList)
+    for iCond=1:length(condition)
+        job.createTask(@rs_project_runsearchlight_nchoosek_CLUSTER, 0,{subList(iSub), condition{iCond}});
+    end
+end
+%% Run the analysis
+job.Tasks
+job.submit();
+%job.wait();
+%job.delete();
